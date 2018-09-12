cbd29b23d55ded5857e0141e214252d90b34da4a
unknown <njrushak@protonmail.com>
2018-09-12 00:07:44 -0700

    pcg hello world

---
+++ Procedural-Gen-Proj-01/Assets/Scripts/HelloWorld.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class HelloWorld : MonoBehaviour {
+
+    public string[] hellos = new string[4] {
+        "Hello World", "Hola Mundo",
+        "Bonjour Le Monde", "Hallo Welt"};
+
+    public TextMeshProUGUI textString;  
+
+	// Use this for initialization
+	void Start () {
+        Random.seed = (int)System.DateTime.Now.Ticks;
+        int randomIndex = Random.Range(0, hellos.Length);
+        textString.text = hellos[randomIndex];
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
